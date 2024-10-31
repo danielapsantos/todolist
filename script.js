51419f652ea48c7c339b34fdd8b9d8f7a6a55093
danielapsantos <daniela.rps09@gmail.com>
2024-10-31 16:26:15 -0300

    adicionado o evento enter

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.getElementById("botao").addEventListener("click", adicionar);
+document.getElementById("item").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); //não vai enviar o formulário
+        adicionar();
+    }
+});
 
-function adicionar(){
+function adicionar() { //adiciona o item na lista
+    console.log("Função adicionar chamada!");
+
     const valor = document.getElementById("item").value;
     if(valor.trim() !== ""){
         const criar = document.createElement("li");
@@ -8,6 +16,7 @@ function adicionar(){
         const checkB = document.createElement("input");
         checkB.type = "checkbox";
 
+        //criar item da lista
         criar.appendChild(checkB);
         criar.appendChild(document.createTextNode(" " + valor));
 
