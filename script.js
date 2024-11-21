24e782fd832262800242f034703d86b260abb2dd
danielapsantos <daniela.rps09@gmail.com>
2024-11-21 17:36:53 -0300

    correção dos botões e largura das listas

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function adicionar() { //adiciona o item na lista
         desc.textContent = descricao ? descricao : "";
         desc.style.display = "block";
         desc.style.fontSize = "0.8em"
-        desc.style.color = "666";
+        desc.style.color = "#666";
 
         container.appendChild(texto);
         container.appendChild(desc);
@@ -39,7 +39,7 @@ function adicionar() { //adiciona o item na lista
                 document.getElementById("concluidos").appendChild(criar);
             } else {
                 texto.classList.remove("riscado")
-                document.getElementById("lista").appendChild(criar);
+                document.getElementById("pendentes").appendChild(criar);
             }
         });
 
@@ -55,13 +55,13 @@ function adicionar() { //adiciona o item na lista
             criar.remove();
         });
 
-        const editarBton = document.createElement("button"); //editar o item
-        editarBton.classList.add("editar");
+        const editarBton = document.createElement("button"); //editar item
+        editarBton.classList.add("editar")
         editarBton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-        </svg>`;
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+            </svg>`;
         editarBton.addEventListener("click", function(){
             if(!editarBton.classList.contains("editando")) {
                 const novaEntrada = document.createElement("input");
@@ -69,8 +69,7 @@ function adicionar() { //adiciona o item na lista
                 novaEntrada.value = texto.textContent.trim();
                 novaEntrada.classList.add("novo");
 
-                criar.insertBefore(novaEntrada, texto);
-                criar.removeChild(texto);
+                container.replaceChild(novaEntrada, texto);
 
                 editarBton.textContent = "Salvar";
                 editarBton.classList.add("editando");
@@ -80,14 +79,13 @@ function adicionar() { //adiciona o item na lista
 
                 if(novoValor !== "") {
                     texto.textContent = " " + novoValor;
-                    criar.insertBefore(texto, novaEntrada);
-                    criar.removeChild(novaEntrada);
+                    container.replaceChild(texto, novaEntrada);
 
                     editarBton.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-                    </svg>`;
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a.5.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                        </svg>`;
                     editarBton.classList.remove("editando");
                 } else {
                     alert("O campo de edição não pode estar vazio.");
@@ -97,12 +95,11 @@ function adicionar() { //adiciona o item na lista
 
         //criar item da lista
         criar.appendChild(checkB);
-        criar.appendChild(texto);
         criar.appendChild(container);
         criar.appendChild(editarBton);
         criar.appendChild(excluirBton);
 
-        document.getElementById("lista").appendChild(criar);
+        document.getElementById("pendentes").appendChild(criar);
 
         document.getElementById("item").value = "";
         document.getElementById("data").value = "";
